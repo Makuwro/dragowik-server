e77b11b9df33682f55c662da3651375d9d788f63
Christian <37517470+Draguwro@users.noreply.github.com>
2021-02-20 22:14:57 -0500

    Update article contributors

--- routes/article/update-article.js
+++ routes/article/update-article.js
@@ -1,37 +1,53 @@
 const db = require("../../database");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
+const fetch = require("node-fetch");
 
 module.exports = (app) => {
   
-  function manageArticle(update, req, res) {
+  async function manageArticle(update, req, res) {
     
+    // Verify the user 
+    const SessionToken = req.header("sessionToken");
+    const UserInfo = SessionToken ? db.prepare("select rowid, * from Users where sessionToken = (?)").get(SessionToken) : undefined;
+    if (!SessionToken || !UserInfo) {
+      res.status(SessionToken ? 401 : 400).json({error: (SessionToken ? "Invalid" : "Missing") + " token"});
+      return;
+    };
+    
+    // Now do some article checks
     const ArticleName = req.params.articleName;
     const ArticleData = update ? db.prepare("select * from Articles where name = (?)").get(ArticleName) : undefined;
-    
     if (!ArticleData && update) {
       res.status(404).json({error: "Article not found"});
       return;
     };
     
     const Source = req.body.source;
-    
     if (!Source) {
       res.status(400).json({error: "No content given"});
     };
     
-    update ? db.prepare("update Articles set source = (?), lastUpdated = current_timestamp where name = (?)").run(Source, ArticleName) : db.prepare("insert into Articles (name, source) values (?,?)").run(ArticleName, Source);
+    // Update the article 
+    const Contributors = update ? JSON.parse(ArticleData.contributors).filter(contributorId => {
+      return contributorId !== UserInfo.rowid;
+    }) : undefined;
     
+    Contributors ? Contributors.unshift(UserInfo.rowid) : undefined;
+    
+    update ? db.prepare("update Articles set source = (?), lastUpdated = current_timestamp, contributors = (?) where name = (?)").run(Source, JSON.stringify(Contributors), ArticleName) : db.prepare("insert into Articles (name, source, contributors) values (?,?,?)").run(ArticleName, Source, JSON.stringify([UserInfo.rowid]));
+    
+    // Success!
     res.sendStatus(update ? 200 : 201);
     
   };
   
-  app.put("/api/article/:articleName", (req, res) => {
-    manageArticle(true, req, res);
+  app.put("/api/article/:articleName", async (req, res) => {
+    await manageArticle(true, req, res);
   });
   
-  app.post("/api/article/:articleName", (req, res) => {
-    manageArticle(false, req, res);
+  app.post("/api/article/:articleName", async (req, res) => {
+    await manageArticle(false, req, res);
   });
   
 };
